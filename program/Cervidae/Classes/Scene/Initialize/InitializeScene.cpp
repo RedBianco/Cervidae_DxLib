74d4fae6c3eaa2a513204a0e7362c6fe4921a3ab
Kashima Akihiro <sectionover@yahoo.co.jp>
2018-10-15 18:23:50 +0900

    [T] 基本エントリーシーンファイル追加

---
+++ program/Cervidae/Classes/Scene/Initialize/InitializeScene.cpp
@@ -0,0 +1,30 @@
+
+
+#include <windows.h>
+
+#include "InitializeScene.h"
+
+//===================================================================================
+//  InitializeScene.cpp
+//  Cervidae
+//
+//  Created by kashima akihiro on 2018/10/15.
+//===================================================================================
+
+
+int APIENTRY  WinMain(	HINSTANCE	hInstance,
+						HINSTANCE	hPrevInstance,
+						LPSTR	lpszCmdLine,
+						int		nCmdShow )
+{
+	return 0;
+}
+
+
+
+
+
+
+
+
+
