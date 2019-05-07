be9cb8e74272c3e5100d32d2fa594c914181ffd7
Kashima Akihiro <sectionover@yahoo.co.jp>
2019-05-08 00:24:09 +0900

    [T] DxLibサウンドライブラリ、動画、音声、BGM管理処理用ファイル追加

---
+++ program/Cervidae/Classes/Libs/DxLib/Sound/dxLibSoundMovieManager.cpp
@@ -0,0 +1,36 @@
+
+#include "DxLib.h"
+#include "../../../Common/CervidaeLib/CLDefine.h"
+#include "../../../Common/CommonList.h"
+#include "dxLibSoundManageConfig.h"
+#include "dxLibSoundMovieManager.h"
+
+//==========================================================================//
+//  DxLibサウンド 動画管理プロセス
+//
+//  Created by kashima akihiro on 2019/05/07.
+//==========================================================================//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ program/Cervidae/Classes/Libs/DxLib/Sound/dxLibSoundVoiceManager.cpp
@@ -0,0 +1,36 @@
+
+#include "DxLib.h"
+#include "../../../Common/CervidaeLib/CLDefine.h"
+#include "../../../Common/CommonList.h"
+#include "dxLibSoundManageConfig.h"
+#include "dxLibSoundVoiceManager.h"
+
+//==========================================================================//
+//  DxLibサウンド 音声ファイル管理プロセス
+//
+//  Created by kashima akihiro on 2019/05/07.
+//==========================================================================//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
