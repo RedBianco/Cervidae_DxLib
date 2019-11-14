fad73b2b4489d1078c9b67794f76cff4c407c182
Kashima Akihiro <sectionover@yahoo.co.jp>
2019-11-15 03:40:27 +0900

    [T] EffekseerForDXLibをVer1.43dに更新

--- program/Cervidae/Libs/Effekseer/Sample/Sample/sample3D.cpp
+++ program/Cervidae/Libs/Effekseer/Sample/Sample/sample3D.cpp
@@ -12,20 +12,23 @@ int sample3D()
 	//描画先を裏画面に変更する。
 	SetDrawScreen(DX_SCREEN_BACK);
 
-	// DirectX9を使用するようにする。(DirectX11も可)
+	// DirectX11を使用するようにする。(DirectX9も可)
 	// Effekseerを使用するには必ず設定する。
-	SetUseDirect3DVersion(DX_DIRECT3D_9);
+	SetUseDirect3DVersion(DX_DIRECT3D_11);
 
 	// DXライブラリを初期化する。
 	if (DxLib_Init() == -1) return -1;
 
 	// Effekseerを初期化する。
 	// 引数には画面に表示する最大パーティクル数を設定する。
-	if (Effkseer_Init(8000) == -1)
+	if (Effekseer_Init(8000) == -1)
 	{
 		DxLib_End();
 		return -1;
 	}
+
+	// ネットワーク機能を使う場合、ここで起動する。60000番のポートで開始する。
+	// Effekseer_StartNetwork(60000);
 
 	// フルスクリーンウインドウの切り替えでリソースが消えるのを防ぐ。
 	// Effekseerを使用する場合は必ず設定する。
